ae55e0808f64e87a3b84bda6079c2d454609595d
Overkane <overkane@mail.ru>
2025-03-15 01:02:02 +0500

    Updated player's sprite, added new animations, changed controls

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,15 +1,18 @@
 extends Area2D
 class_name Player
 
-const _MIN_SPEED = 50.0
+const _MIN_SPEED = 50
 const _MAX_SPEED = _MIN_SPEED * 2
-const _INITIAL_SPEED = (_MIN_SPEED + _MAX_SPEED) / 2.0
+const _INITIAL_SPEED = (_MIN_SPEED + _MAX_SPEED) / 2
 
 @export var _baseStats: BaseStats
+
+var isDead := false
 
 var _currentSpeed := _INITIAL_SPEED
 @warning_ignore("narrowing_conversion")
 var _currentPathNumber: int = round(GameConstants.NUMBER_OF_PATHES / 2.0)
+var _currentMoveAnimation := "walk"
 
 
 func _ready() -> void:
@@ -20,27 +23,51 @@ func _ready() -> void:
 	%ArmorLabel.text = str(_baseStats.currentArmor)
 
 func _physics_process(delta: float) -> void:
-	# Base horizontal movement
-	global_position += Vector2.RIGHT * _currentSpeed * delta
-
-	# Vertical movement
-	var verticalVector := Vector2.ZERO
-	if Input.is_action_just_pressed("left") and _currentPathNumber > 1:
-		verticalVector = Vector2.UP
-		_currentPathNumber -= 1
-	elif Input.is_action_just_pressed("right") and _currentPathNumber < GameConstants.NUMBER_OF_PATHES:
-		verticalVector = Vector2.DOWN
-		_currentPathNumber += 1
-	global_position += verticalVector * GameConstants.DISTANCE_BETWEEN_PATHES
+	if not isDead:
+		_applyMovement(delta)
 
 
 func changeHP(value: int) -> void:
 	_baseStats.currentHP += value
 	%HPLabel.text = str(_baseStats.currentHP)
 	if _baseStats.currentHP <= 0:
-		queue_free() # TODO lose condition
+		isDead = true
+		$AnimatedSprite2D.play("death")
+		await $AnimatedSprite2D.animation_finished
+		print("game end") # TODO lose condition
+
+func changeSpeed(value: int) -> void:
+	_currentSpeed += value
+	if _currentSpeed >= (_INITIAL_SPEED + _MAX_SPEED) / 2:
+		_currentMoveAnimation = "run"
+	else:
+		_currentMoveAnimation = "walk"
+	$AnimatedSprite2D.play(_currentMoveAnimation)
+
+
+func _applyMovement(delta: float) -> void:
+	# Base horizontal movement
+	global_position += Vector2.RIGHT * _currentSpeed * delta
+
+	# Vertical movement
+	var verticalVector := Vector2.ZERO
+	if Input.is_action_just_pressed("move_up") and _currentPathNumber > 1:
+		verticalVector = Vector2.UP
+		_currentPathNumber -= 1
+	elif Input.is_action_just_pressed("move_down") and _currentPathNumber < GameConstants.NUMBER_OF_PATHES:
+		verticalVector = Vector2.DOWN
+		_currentPathNumber += 1
+	global_position += verticalVector * GameConstants.DISTANCE_BETWEEN_PATHES
 
 
 func _onAreaEntered(body: Node2D) -> void:
 	assert(body.has_method("interact"), str(body) + " doesn't have 'interact()' method.")
 	body.interact()
+
+	if not isDead:
+		var attackVariant := randi_range(1, 3)
+		$AnimatedSprite2D.play("attack%s" % attackVariant)
+		await $AnimatedSprite2D.animation_finished
+		# In case dead during attack animation
+		if not isDead:
+			$AnimatedSprite2D.play(_currentMoveAnimation)
