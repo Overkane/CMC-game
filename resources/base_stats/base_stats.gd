caa7bf802c4e29de175ffd364c032e28dbaf2b4f
Overkane <overkane@mail.ru>
2025-03-15 14:32:46 +0500

    Fixed hp stat and changed player stats

--- resources/base_stats/base_stats.gd
+++ resources/base_stats/base_stats.gd
@@ -2,6 +2,7 @@ extends Resource
 class_name BaseStats
 
 @export var maxHP := 100
-@export var currentHP := maxHP
 @export var currentArmor := 1
 @export var currentDamage := 1
+
+var currentHP := maxHP
